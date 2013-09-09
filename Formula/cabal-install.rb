20b6935d637861c7c28d1a46a152c4e5a9d276bd
Darin Morrison <darinmorrison+git@gmail.com>
2013-09-09 12:16:14 -0600

    cabal-install: (hopefully) fix a problem where existing ~/.ghc can cause install to fail

--- Formula/cabal-install.rb
+++ Formula/cabal-install.rb
@@ -9,6 +9,11 @@ class CabalInstall < Formula
 
   conflicts_with 'haskell-platform'
 
+  def patches
+    # force bootstrap.sh to ignore the user package db under ~/.ghc
+    DATA
+  end
+
   def install
     # create a sandbox to encapsulate the bootstrap output
     sandbox = "#{Dir.getwd}/#{`mktemp -d .sandbox-XXXX`.chuzzle}"
@@ -30,3 +35,19 @@ class CabalInstall < Formula
     bash_completion.install 'bash-completion/cabal'
   end
 end
+
+__END__
+diff --git a/bootstrap.sh b/bootstrap.sh
+index e6eb479..69315b4 100755
+--- a/bootstrap.sh
++++ b/bootstrap.sh
+@@ -86,7 +86,7 @@ GHC_PKG_VER=`${GHC_PKG} --version | cut -d' ' -f 5`
+ 
+ # Cache the list of packages:
+ echo "Checking installed packages for ghc-${GHC_VER}..."
+-${GHC_PKG} list --global ${SCOPE_OF_INSTALLATION} > ghc-pkg.list \
++${GHC_PKG} list --global ${SCOPE_OF_INSTALLATION} --no-user-package-db > ghc-pkg.list \
+   || die "running '${GHC_PKG} list' failed"
+ 
+ # Will we need to install this package, or is a suitable version installed?
+
